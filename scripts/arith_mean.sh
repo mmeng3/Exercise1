178a19411e0f2e109b57a8a2eab18abf0f968427
He Tianyi <hetianyi@HetekiiMac.local>
2017-03-01 00:25:26 -0600

    Esther - commit to branch

---
+++ scripts/arith_mean.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Computes the arithmetic mean
+#1 Path to file containing numeric values one per line
+arith_mean() {
+
+  sum=0                     # Hold summation
+  fname=$1                  # File to take estimate of
+  count=0                   # Hold count of number of lines
+
+  while read line
+  do
+    sum=$((sum+line))      # Sum
+    count=$((count+1))
+  done < "$fname"
+
+  if [ "$count" != 0 ]; then
+    mu=$((sum/count))
+    echo "Sum = $sum, Count = $count, Mean = $mu"
+  else
+    echo "Please supply a file with more than 1 line of numerical input."
+  fi
+  
+}
+
+arith_mean $1             # Function call
